abbcba4c4790fe94131a0115ee5862b6ff74e8be
andrealeon-2 <andrea.leon270@students.cmcss.net>
2026-03-05 08:00:52 -0600

    hi

--- task3.py
+++ task3.py
@@ -1,7 +1,7 @@
 # Task 3 — The Guard
 guard_awake = False
 
-if not guard_alert:
+if not guard_awake:
     outcome = "Shadow: You slip past the guard unnoticed."
 else:
     outcome = "Doom: The guard spots you and raises the alarm."
